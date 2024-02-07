abb8678ac5110ef23e5205cdf2a8ffb7969898a2
zoulikha <zaz13@me.com>
2024-02-07 01:20:49 +0100

    add données json

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,28 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price, id } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    // Attribution d'un identifiant unique à l'article
+    article.id = `photographer-${id}`;
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    const h3Location = document.createElement("h3");
+    h3Location.textContent = `${city}, ${country}`;
+    const p = document.createElement("p");
+    p.textContent = tagline;
+    const prices = document.createElement("p");
+    prices.textContent = price + "€/jours";
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(h3Location);
+    article.appendChild(p);
+    article.appendChild(prices);
+    return article;
+  }
+  return { name, picture, id, getUserCardDOM };
 }
