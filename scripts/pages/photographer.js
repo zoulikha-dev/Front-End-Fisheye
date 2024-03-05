8c43e1c55175a8d8d9f436f071a9b28c8bd42334
zoulikha <zaz13@me.com>
2024-03-05 01:28:17 +0100

    ...

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,82 @@
-//Mettre le code JavaScript lié à la page photographer.html
+//Récupération de l'id du photographe a partir de l'url
+const urlParams = new URLSearchParams(window.location.search);
+const photographerId = urlParams.get("id");
+
+//Construction de l'url de l'api avec l'ID du photographe
+const apiUrl = `data/photographers.json`;
+
+//Récupération des données du photographe a partir de l'api
+fetch(apiUrl)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Erreur");
+    }
+    //Convertion de la réponse en JSON
+    return response.json();
+  })
+  .then((data) => {
+    //Trouver le photographe avec l'id qui correspond
+    const photographer = data.photographers.find(
+      (photographer) => photographer.id === parseInt(photographerId)
+    );
+
+    if (photographer) {
+      //Afficher les détails du photographe
+      photographerDetails(photographer);
+
+      //Filtrer les médias pour obtenir ceux du photographe sélectionné
+      const photographerMedia = data.media.filter(
+        (media) => media.photographerId === photographer.id
+      );
+
+      photographerMediaDetails(photographerMedia, photographer);
+    } else {
+      console.error("Photographe non trouvé");
+    }
+  })
+  .catch((error) => {
+    //gerer les erreurs
+    console.error("Erreur lors de la récupération des données fetch:", error);
+  });
+
+//Fonction pour afficher les détails du photographe
+function photographerDetails(photographer) {
+  const photographHeaderDiv = document.querySelector(".photograph-header");
+
+  // Création des différentes divs pour organiser les éléments
+  const photographerDetailsDiv = document.createElement("div");
+  photographerDetailsDiv.classList.add("photographer-details");
+  photographerDetailsDiv.innerHTML = `
+    <h2>${photographer.name}</h2> 
+    <h3>${photographer.city}, ${photographer.country}</h3>
+    <p>${photographer.tagline}
+    <img src="assets/photographers/${photographer.portrait}" alt="Portrait du photographe">`;
+
+  console.log(photographer);
+  photographHeaderDiv.appendChild(photographerDetailsDiv);
+}
+
+// Fonction pour afficher les medias du photographe
+function photographerMediaDetails(photographerMedia, photographer) {
+  // Convertir le nom du photographe en un format utilisable pour le dossier
+  const photographerFolder = photographer.name.replace("_", " ");
+
+  const photographerDetailsMediaDiv = document.createElement("div");
+  photographerDetailsMediaDiv.classList.add("pictures");
+
+  photographerMedia.forEach((media) => {
+    const mediaElement = document.createElement(media.image ? "img" : "video");
+    if (media.image) {
+      mediaElement.src = `assets/images/${photographerFolder}/${media.image}`;
+      mediaElement.alt = media.title;
+    } else {
+      mediaElement.src = `assets/images/${photographerFolder}/${media.video}`;
+      mediaElement.type = "video/mp4";
+      mediaElement.controls = true; //ajout des controle de lecture pour les vidéos
+    }
+    photographerDetailsMediaDiv.appendChild(mediaElement);
+  });
+
+  const mainElement = document.getElementById("main");
+  mainElement.appendChild(photographerDetailsMediaDiv);
+}
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -7,24 +7,46 @@ function photographerTemplate(data) {
     const article = document.createElement("article");
     // Attribution d'un identifiant unique à l'article
     article.id = `photographer-${id}`;
+
+    const lien = document.createElement("a");
+    lien.setAttribute("href", `photographer.html?id=${id}`);
+
+    //Creation de la div pour l'image
+    const divImg = document.createElement("div");
+    divImg.classList.add("image-container");
+
     const img = document.createElement("img");
     img.setAttribute("src", picture);
     img.setAttribute("alt", "portrait du photographe");
+
     const h2 = document.createElement("h2");
     h2.textContent = name;
+
+    lien.appendChild(img);
+    lien.appendChild(h2);
+
+    // Ajouter l'image et le titre dans le lien
+    divImg.appendChild(img);
+    divImg.appendChild(h2);
+
     const h3Location = document.createElement("h3");
     h3Location.textContent = `${city}, ${country}`;
+
     const pTagline = document.createElement("p");
     pTagline.textContent = tagline;
     pTagline.classList.add("taglinePhotographer");
+
     const prices = document.createElement("p");
     prices.textContent = price + "€/jours";
     prices.classList.add("pricePhotographer");
-    article.appendChild(img);
-    article.appendChild(h2);
+
+    // Ajout du lien dans l'article
+    article.appendChild(lien);
+    lien.appendChild(divImg);
     article.appendChild(h3Location);
     article.appendChild(pTagline);
     article.appendChild(prices);
+
     return article;
   }
   return { name, picture, id, getUserCardDOM };
